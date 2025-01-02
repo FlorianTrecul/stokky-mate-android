336173583a7f13fcc96bb260077d0a6add03efe2
FlorianTrecul <trecul.florian@gmail.com>
2025-01-02 10:01:37 +0100

    feat: added Detekt to the project and integrated it into the CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,3 +9,33 @@ buildscript {
         classpath(libs.kotlin.gradle.plugin)
     }
 }
+
+plugins {
+    id(libs.plugins.detekt.get().pluginId) version libs.versions.detekt
+}
+
+val projectStructure = file(projectDir)
+val configFile = files("$rootProject/../config/detekt/detekt.yml")
+val kotlinFiles = "**/*.kt"
+val resourcesFiles = "**/resources/**"
+val buildFiles = "**/build/**"
+val autoFix = project.hasProperty("detektAutoFix")
+
+tasks.register<io.gitlab.arturbosch.detekt.Detekt>("detektAllModules") {
+
+    description = "Runs Detekt analysis across all modules in the project"
+    parallel = true
+    ignoreFailures = false
+    autoCorrect = autoFix
+    buildUponDefaultConfig = true
+    setSource(file(projectDir))
+    config.setFrom(configFile)
+    include("**/*.kt")
+    exclude("**/resources/**", "**/build/**")
+
+    reports {
+        html.required.set(true)
+        xml.required.set(false)
+        txt.required.set(false)
+    }
+}
